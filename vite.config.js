7756ae6c960dd98f15c6c6c3728a4cf4e5fc5c3f
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-02 11:06:07 +0400

    minify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   homepage: "https://roma252004sky.github.io/GithubPageTest/",
-  base: "https://roma252004sky.github.io/GithubPageTest/"
+  base: "https://roma252004sky.github.io/GithubPageTest/",
+  build: {
+    minify: 'esbuild',
+    esbuild: {
+      minifyWhitespace: true,
+      minifyIdentifiers: true,
+      minifySyntax: true,
+      legalComments: 'none'
+    }
+  }
 })
+
