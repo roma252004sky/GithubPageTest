c05d83bb9bd4ae1478a5d575a159b5636a7f2fac
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-02 10:52:30 +0400

    Revert "static"
    This reverts commit 7f2edcbfa6013288a7c4335a986b1d1376df64fb.

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  homepage: "https://roma252004sky.github.io/GithubPageTest/",
+  base: "https://roma252004sky.github.io/GithubPageTest/"
+})
