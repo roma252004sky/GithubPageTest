73f40811d7747988ac2424a89df187527dae5142
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-05 09:40:00 +0400

    minify

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import React from "react";
 
 createRoot(document.getElementById('root')).render(
+    // eslint-disable-next-line react/react-in-jsx-scope
   <StrictMode>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
     <App />
   </StrictMode>,
 )
