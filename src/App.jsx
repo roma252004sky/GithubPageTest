cef81c71fdc026befe00b1e6dbdedabe2090b1f1
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-05 08:50:36 +0400

    minify

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); const COUNT = 0
+
 
   return (
     <>
-      <div><<<<<<<<<<<
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
