0b8ec8e968c29f1ba4592af28568d8ff88e8bca6
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-05 09:43:21 +0400

    minify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  homepage: "https://roma252004sky.github.io/GithubPageTest/",
   base: "https://roma252004sky.github.io/GithubPageTest/",
 })
 
